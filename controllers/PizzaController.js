59be8cae1884c15972041168ae0aac3eda076d35
Beatriz Medina <bee.a2@hotmail.com>
2021-07-02 15:49:52 -0300

    Tarefa: Atualizacao do controller

--- controllers/PizzaController.js
+++ controllers/PizzaController.js
@@ -1,9 +1,14 @@
-const pizzas = require("./database/pizzas.json");
+const pizzas = require("../database/pizzas.json");
+const PizzaModel = require('../models/Pizza');
+
 const { v4 } = require("uuid"); // const uuid = require('uuid'); // serve para gerar id aleatorios, o v4 é uma função interna que executa o codigo
 const fs = require("fs"); 
 
 const PizzaController = {
-    listar: (req, res) => res.json(pizzas);
+    listar: (req, res) => {
+        const pizzas = PizzaModel.findAll();
+        res.json(pizzas)
+    },
 
     criarUmaPizza: (req, res) => {
         const { sabor, categoria, preco } = req.body;
@@ -15,38 +20,19 @@ const PizzaController = {
             preco,
         };
 
-        pizzas.push(pizzaNova);
-
-        fs.writeFileSync("./database/pizzas.json", JSON.stringify(pizzas)); // Serve para escrever a atualização no json que está os dados iniciais
+        PizzaModel.create(pizzaNova);
 
         res.status(201).json(pizzaNova);
     },
 
     editarUmaPizza: (req, res) => {
-        const { id } = req.params;
-        const { sabor, categoria, preco } = req.body;
-
-        const pizzaEncontrada = pizzas.find((pizza) => pizza.id === id);
-
-        if (!pizzaEncontrada) {
-            return res.status(400).json({ mensagem: "Pizza não encontrada" });
-        }
-
-        pizzaEncontrada.sabor = sabor;
-        pizzaEncontrada.categoria = categoria;
-        pizzaEncontrada.preco = preco;
-
-        fs.writeFileSync("./database/pizzas.json", JSON.stringify(pizzas));
+        PizzaModel.update(pizzaEncontrada);
 
         return res.json(pizzaEncontrada);
     },
 
     deletarUmaPizza: (req, res) => {
-        const {id} = req.params;
-
-        const index = pizzas.findIndex((pizza) => pizza.id === id);
-
-        pizzas.splice(index, 1); //remove 1 elemento do index
+        PizzaModel.destroy;
 
         res.status(204).send();
     },
